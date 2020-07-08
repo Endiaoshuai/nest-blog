b846b5b8b14fc5fa351a91b5fe510db1c6f7850e
Endiaoshuai <handsomezhaoshuai@gmail.com>
2020-07-08 11:27:12 +0800

    fix: 删除一切与 gql 有关的

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -3,14 +3,13 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { UserModule } from "../user/user.module";
 import { PostController } from "./post.controller";
-import { PostDataLoader } from "./post.data-loader";
 import { Post } from "./post.entity";
 import { PostService } from "./post.service";
 
 @Module({
   imports: [TypeOrmModule.forFeature([Post]), forwardRef(() => UserModule)],
-  providers: [PostService, PostDataLoader],
-  exports: [PostService, PostDataLoader],
+  providers: [PostService],
+  exports: [PostService],
   controllers: [PostController],
 })
 export class PostModule {}
--- src/user/decorators/current-user.decorator.ts
+++ src/user/decorators/current-user.decorator.ts
@@ -1,13 +1,10 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common";
-import { GqlExecutionContext } from "@nestjs/graphql";
 
 import { User } from "../user.entity";
 
 export const CurrentUser = createParamDecorator(
   (data, ctx: ExecutionContext) => {
-    const request =
-      GqlExecutionContext.create(ctx).getContext().req ||
-      ctx.switchToHttp().getRequest();
+    const request = ctx.switchToHttp().getRequest();
 
     return request.user instanceof User ? request.user : null;
   }
